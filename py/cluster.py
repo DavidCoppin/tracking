eaa16c2957e1ad7a5fcbade900f7f3dd1de632b9
Alex Pletzer <alexander@gokliya.net>
2018-03-07 22:34:05 +0000

    work in progress

--- py/cluster.py
+++ py/cluster.py
@@ -1,4 +1,5 @@
 import numpy
+import math
 
 class Cluster:
 
@@ -23,13 +24,15 @@ class Cluster:
     
     def update(self):
 
-        self.centre, self.ellipsisA, self.ellipsisB, self.angle = \
+        if len(self.cells) > 0:
+
+            self.centre, self.ellipsisA, self.ellipsisB, self.angle = \
                  self.getEllipsis()
 
-        self.iMin = numpy.minimum([c[0] for c in self.cells])
-        self.jMin = numpy.minimum([c[1] for c in self.cells])
-        self.iMax = numpy.maximum([c[0] for c in self.cells])
-        self.jMax = numpy.minimum([c[1] for c in self.cells])
+            self.iMin = numpy.minimum([c[0] for c in self.cells])
+            self.jMin = numpy.minimum([c[1] for c in self.cells])
+            self.iMax = numpy.maximum([c[0] for c in self.cells])
+            self.jMax = numpy.minimum([c[1] for c in self.cells])
 
     def merge(self, otherCluster):
         self.cells.union(otherCluster.cells)
@@ -44,18 +47,32 @@ class Cluster:
         if otherCluster.jMax < self.jMin or otherCluster.jMin > self.jMax:
             return res
 
-        # the clusters overlap if the ellipses overlap.
-        # TO IMPLEMENT
+        # the clusters one centre is inside the other ellipse
+        if self.isPointInsideEllipse(self.centre, 
+                otherCluster.centre, 
+                otherCluster.ellipsisA, otherCluster.ellipsisB,
+                otherCluster.ellipsisAngle):
+            return True
 
-        return True
+        if self.isPointInsideEllipse(otherCluster.centre, 
+                self.centre, 
+                self.ellipsisA, self.ellipsisB,
+                self.ellipsisAngle):
+            return True
+
+        return False
 
     def toArray(self, bounds=[]):
         """
-        Convert to numpy array
+        Convert cluster to numpy (dense) array
         @bounds (iMin, iMax, jMin, jMax)
         @return array of coordinates, array of zeros and ones
         """
-        iMin, iMax, jMin, jMax = self.iMin, self.iMax, self.jMin, self.jMax
+        if len(self.cells) <= 0:
+            # no op
+            return numpy.array([]), numpy.array([]), numpy.array([])
+
+        iMin, jMin, jMin, jMax = self.iMin, self.iMax, self.jMin, self.jMax
         if bounds:
             iMin, iMax, jMin, jMax = bounds
         iCoords = numpy.arange(iMin, iMax + 1)
@@ -73,6 +90,7 @@ class Cluster:
         iCoords, jCoords, ijValues = self.toArray()
         pylab.matshow(ijValues)
         pylab.show()
+
          
     def write_file(self, filename):
         """
@@ -82,20 +100,29 @@ class Cluster:
         import netCDF4
         iCoords, jCoords, ijValues = self.toArray()
         nc = netCDF4.Dataset(filename, 'w', format="NETCDF4")
-        iDim = nc.createDimension('i', size=ijCoords.shape[0])
-        jDim = nc.createDimension('j', size=ijCoords.shape[1])
-        var = nc.createVariable('nb', 'i4', dimensions=(iDim, jDim))
-        iDim[:] = iCoords
-        jDim[:] = jCoords
-        var[...] = ijValues
+        iDim = nc.createDimension('iDim', size=iCoords.shape[0])
+        jDim = nc.createDimension('jDim', size=jCoords.shape[0])
+        iVar = nc.createVariable('i', 'i4', dimensions=('iDim',))
+        jVar = nc.createVariable('j', 'i4', dimensions=('jDim',))
+        nbVar = nc.createVariable('nb', 'i4', dimensions=('iDim', 'jDim'))
+        iVar[:] = iCoords
+        jVar[:] = jCoords
+        nbVar[:, :] = ijValues
         nc.close()
 
 
     def getNumCells(self):
+        """
+        Get the number of cells
+        @return number
+        """
         return len(self.cells)
 
     def getEllipsis(self):
-
+        """
+        Get the ellipsis parameters that best represent this cluster
+        @return centre, radius1, radius2, angle
+        """
         centre = self.__getCentre()
         inertia = numpy.zeros((2, 2), numpy.float64)
         inertia[0, 0] = numpy.sum([(c[0] - centre[0])^2 for c in self.cells])
@@ -104,7 +131,7 @@ class Cluster:
         inertia[1, 1] = numpy.sum([(c[1] - centre[1])^2 for c in self.cells])
 
         # diagonalize
-        eigenvals, eigenvecs = numpy.linalg.eig(intertia)
+        eigenvals, eigenvecs = numpy.linalg.eig(inertia)
         a = numpy.sqrt(eigenvals[0])
         b = numpy.sqrt(eigenvals[1])
         vecA = eigenvecs[:, 0]
@@ -125,6 +152,10 @@ class Cluster:
             jCentre = numpy.sum([c[1] for c in self.cells]) / float(n)
         return numpy.array([iCentre, jCentre])
 
+    def isPointInsideEllipse(self, point, 
+        ellipsisCentre, ellipsisA, ellipsisB, ellipsisAngle):
+        pass
+
 
 
 #############################################################################################
@@ -132,8 +163,17 @@ def test0():
     # should be able to create a cluster with nothing in it
     cluster = Cluster()
     cluster.update()
+    # not sure why writing a zero dimensioned variable isnot working with netcdf4
+    #cluster.write_file('test0.nc')
+
+def test1():
+    # should be able to create a cluster with nothing in it
+    cluster = Cluster({(-1, -2)})
+    cluster.update()
     cluster.write_file('test0.nc')
+
 
 if __name__ == '__main__':
     test0()
+    test1()
 
