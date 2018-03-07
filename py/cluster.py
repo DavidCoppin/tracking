edce022279e29ab99bd6450d20cbdf3a61d6afb6
Alex Pletzer <alexander@gokliya.net>
2018-03-07 21:20:15 +0000

    work in progress

--- py/cluster.py
+++ py/cluster.py
@@ -7,15 +7,25 @@ class Cluster:
         Constructor 
         @param cells set of (i,j) tuples
         """
+        # set of i,j cells 
         self.cells = cells
+
+        # location of the centre of cells
         self.centre = []
+
+        # ellipsis parameters
         self.ellipsisA = None
         self.ellipsisB = None
         self.ellipsisAngle = None
+
+        # min/max indices of the box containing the set of points
         self.iMin, self.jMin, self.iMax, self.jMax = None, None, None, None
     
     def update(self):
-        self.centre, self.ellipsisA, self.ellipsisB, self.angle = self.getEllipsis()
+
+        self.centre, self.ellipsisA, self.ellipsisB, self.angle = \
+                 self.getEllipsis()
+
         self.iMin = numpy.minimum([c[0] for c in self.cells])
         self.jMin = numpy.minimum([c[1] for c in self.cells])
         self.iMax = numpy.maximum([c[0] for c in self.cells])
@@ -27,8 +37,6 @@ class Cluster:
 
     def overlaps(self, otherCluster):
 
-        # DAVID TO VERIFY!!!!
-
         # quick check...
         res = False
         if otherCluster.iMax < self.iMin or otherCluster.iMin > self.iMax:
@@ -36,29 +44,55 @@ class Cluster:
         if otherCluster.jMax < self.jMin or otherCluster.jMin > self.jMax:
             return res
 
-        # the clusters overlap if the ellipses overlap
-        
-
+        # the clusters overlap if the ellipses overlap.
+        # TO IMPLEMENT
 
         return True
 
+    def toArray(self, bounds=[]):
+        """
+        Convert to numpy array
+        @bounds (iMin, iMax, jMin, jMax)
+        @return array of coordinates, array of zeros and ones
+        """
+        iMin, iMax, jMin, jMax = self.iMin, self.iMax, self.jMin, self.jMax
+        if bounds:
+            iMin, iMax, jMin, jMax = bounds
+        iCoords = numpy.arange(iMin, iMax + 1)
+        jCoords = numpy.arange(jMin, jMax + 1)
+        ijValues = numpy.zeros((len(iCoords), len(jCoords)), numpy.int32)
+        ijValues[ (c for c in self.cells) ] = 1
+
+        return iCoords, jCoords, ijValues
+
+    def show(self):
+        """
+        Plots the cluster
+        """
+        from matplotlib import pylab
+        iCoords, jCoords, ijValues = self.toArray()
+        pylab.matshow(ijValues)
+        pylab.show()
          
     def write_file(self, filename):
-        pass
+        """
+        Write to netcdf file
+        @param filename file name
+        """
+        import netCDF4
+        iCoords, jCoords, ijValues = self.toArray()
+        nc = netCDF4.Dataset(filename, 'w', format="NETCDF4")
+        iDim = nc.createDimension('i', size=ijCoords.shape[0])
+        jDim = nc.createDimension('j', size=ijCoords.shape[1])
+        var = nc.createVariable('nb', 'i4', dimensions=(iDim, jDim))
+        iDim[:] = iCoords
+        jDim[:] = jCoords
+        var[...] = ijValues
+        nc.close()
+
 
     def getNumCells(self):
         return len(self.cells)
-
-    def __getCentre(self):
-        """
-        Get the barycentric centre
-        """
-        iCentre, jCentre = None, None
-        n = len(self.cells)
-        if n > 0:
-            iCentre = numpy.sum([c[0] for c in self.cells]) / float(n)
-            jCentre = numpy.sum([c[1] for c in self.cells]) / float(n)
-        return numpy.array([iCentre, jCentre])
 
     def getEllipsis(self):
 
@@ -78,19 +112,28 @@ class Cluster:
 
         return centre, a, b, angle
 
+# private methods 
+
+    def __getCentre(self):
+        """
+        Get the barycentric centre
+        """
+        iCentre, jCentre = None, None
+        n = len(self.cells)
+        if n > 0:
+            iCentre = numpy.sum([c[0] for c in self.cells]) / float(n)
+            jCentre = numpy.sum([c[1] for c in self.cells]) / float(n)
+        return numpy.array([iCentre, jCentre])
+
 
 
 #############################################################################################
-def test1():
-    b1 = Blob(1, {(1,)})
-    b2 = Blob(1, {(2,)})
-    assert(b1.overlaps(b1))
-    assert(not b1.overlaps(b2))
-    assert(b1.shift(0, 1).overlaps(b2))
-    assert(not b1.shift(0, -1).overlaps(b2))
-    assert(b1.merge(b2)) # yes can be merged
-    assert(b1.getNumCells() == 2)
+def test0():
+    # should be able to create a cluster with nothing in it
+    cluster = Cluster()
+    cluster.update()
+    cluster.write_file('test0.nc')
 
 if __name__ == '__main__':
-    test1()
+    test0()
 
