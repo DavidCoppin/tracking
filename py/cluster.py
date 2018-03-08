13d02b15127898a72dbf06ddfc227f09b35e8624
Alex Pletzer <alexander@gokliya.net>
2018-03-08 22:26:16 +0000

    simplified cluster

--- py/cluster.py
+++ py/cluster.py
@@ -1,5 +1,6 @@
 import numpy
 import math
+from ellipsis import Ellipsis
 
 class Cluster:
 
@@ -11,16 +12,9 @@ class Cluster:
         # set of i,j cells 
         self.cells = cells
 
-        # location of the centre of cells
-        self.centre = []
-
-        # ellipsis parameters
-        # radius 1
-        self.ellipsisA = None
-        # radius 2
-        self.ellipsisB = None
-        # angle between pricipal first axis and i coordinate
-        self.ellipsisAngle = None
+        # ellipsis representing the "average" distribution
+        # of cells
+        self.ellpsis = None
 
         # min/max indices of the box containing the set of points
         self.box = [[None, None], [None, None]]
@@ -38,8 +32,7 @@ class Cluster:
 
         if len(self.cells) > 0:
 
-            self.centre, self.ellipsisA, self.ellipsisB, self.ellipsisAngle = \
-                 self.getEllipsis()
+            self.ellipsis = Ellipsis(self.cells)
 
             for dim in range(0, 2):
                 self.box[0][dim] = numpy.min([c[dim] for c in self.cells])
@@ -73,19 +66,14 @@ class Cluster:
             noOverlap &= self.box[0][dim] > otherCluster.box[1][dim]
 
         if noOverlap:
+            # the boxes are disjoint, no chance that there is overlap
             return False
 
         # the clusters one centre is inside the other ellipse
-        if self.isPointInsideEllipse(self.centre, 
-                otherCluster.centre, 
-                otherCluster.ellipsisA, otherCluster.ellipsisB,
-                otherCluster.ellipsisAngle):
+        if self.ellipsis.isPointInside(otherCluster.ellipsis.getCentre()):
             return True
 
-        if self.isPointInsideEllipse(otherCluster.centre, 
-                self.centre, 
-                self.ellipsisA, self.ellipsisB,
-                self.ellipsisAngle):
+        if otherCluster.ellipsis.isPointInside(self.ellipsis.getCentre()):
             return True
 
         return False
@@ -108,33 +96,6 @@ class Cluster:
         nbVar[:, :] = ijValues
         nc.close()
 
-
-    def getEllipsis(self):
-        """
-        Get the ellipsis parameters that best represent this cluster
-        @return centre, radius1, radius2, angle
-        """
-        centre = self.getCentre()
-        inertia = numpy.zeros((2, 2), numpy.float64)
-        inertia[0, 0] = numpy.sum([(c[0] - centre[0])**2 for c in self.cells])
-        inertia[0, 1] = numpy.sum([(c[0] - centre[0])*(c[1] - centre[1]) for c in self.cells])
-        inertia[1, 0] = inertia[0, 1]
-        inertia[1, 1] = numpy.sum([(c[1] - centre[1])**2 for c in self.cells])
-
-        # diagonalize
-        eigenvals, eigenvecs = numpy.linalg.eig(inertia)
-        # number of masses
-        nm = float(len(self.cells))
-        # average radii from the centre
-        a = numpy.sqrt(eigenvals[0] / nm)
-        b = numpy.sqrt(eigenvals[1] / nm)
-        # orientation
-        vecA = eigenvecs[:, 0]
-        angle = math.atan2(vecA[1], vecA[0])
-
-        return centre, a, b, angle
-
-
     def toArray(self, bounds=[]):
         """
         Convert this cluster to numpy (dense) array
@@ -156,100 +117,16 @@ class Cluster:
 
         return iCoords, jCoords, ijValues
 
-
-    def getEllipseAsPolyline(self, numSegments=32):
-        """
-        Return the ellipsis as a segmented line
-        @return iPts, jPts arrays
-        """
-        # rotation of the i j coords to the principal axes of the inertia matrix
-        cosa = math.cos(self.ellipsisAngle)
-        sina = math.sin(self.ellipsisAngle)
-        transf = numpy.array([[cosa, sina], [-sina, cosa]])
-
-        iPts, jPts = [], []
-        dt = 2 * math.pi / float(numSegments)
-        for i in range(numSegments + 1):
-            th = i * dt
-            x = self.ellipsisA * math.cos(th)
-            y = self.ellipsisB * math.sin(th)
-            # rotate back to i,j coordinates
-            ij = transf.dot([x, y])
-            ij += self.centre
-            iPts.append(ij[0])
-            jPts.append(ij[1])
-
-        return iPts, jPts
-
-
     def __repr__(self):
         """
         Print object
         """
         res = """
-        Cluster: num cells = {} box = {} ellipsis centre = {} a = {} b = {} angle = {}
+        Cluster: num cells = {} box = {} ellipsis centre = {} a = {} b = {} transf = {}
         """.format(len(self.cells), self.box, \
-            self.centre, self.ellipsisA, self.ellipsisB, self.ellipsisAngle)
+            self.ellipsis.centre, self.ellipsis.a, self.ellipsis.b, \
+            self.ellipsis.ij2AxesTransf)
         return res
-
-
-    def show(self):
-        """
-        Plots the cluster
-        """
-        from matplotlib import pylab
-        bounds = [[self.box[0][1] - 1, self.box[0][1] - 1], 
-                  [self.box[1][0] + 1, self.box[1][1] + 1]]
-        iCoords, jCoords, ijValues = self.toArray(bounds=bounds)
-
-        # pcolormesh wants nodal coordinates, data are cell centred
-        iBnds = list(iCoords - 0.5) + [iCoords[-1] + 0.5]
-        jBnds = list(jCoords - 0.5) + [jCoords[-1] + 0.5]
-
-        # show the cluster, note the swap of indices with transpose
-        pylab.pcolormesh(iBnds, jBnds, numpy.transpose(ijValues))
-
-        # show the ellipsis
-        iPts, jPts = self.getEllipseAsPolyline()
-        pylab.plot(iPts, jPts, 'c-')
-        pylab.xlabel('i')
-        pylab.ylabel('j')
-        pylab.show()
-
-# private methods 
-
-    def getCentre(self):
-        """
-        Get the barycentric centre
-        """
-        iCentre, jCentre = None, None
-        n = len(self.cells)
-        if n > 0:
-            iCentre = numpy.sum([c[0] for c in self.cells]) / float(n)
-            jCentre = numpy.sum([c[1] for c in self.cells]) / float(n)
-        return numpy.array([iCentre, jCentre])
-
-    def isPointInsideEllipse(self, point, 
-               ellipsisCentre, ellipsisA, ellipsisB, ellipsisAngle):
-        """
-        Check if a point is inside an ellipse
-        """
-        
-        # subtract the centre
-        pt = point - self.centre
-
-        # rotate to the principal axes of the ellipsis
-        cosa = math.cos(ellipsisAngle)
-        sina = math.sin(ellipsisAngle)
-        # Note: this is the inverse transformation
-        invTransf = numpy.array([[cosa, -sina], [sina, cosa]])
-        # distance of point to principal axes
-        ptPrimeAbs = abs(invTransf.dot(pt))
-
-        if (ptPrimeAbs[0] < self.ellipsisA) and (ptPrimeAbs[1] < self.ellipsisB):
-            return True
-
-        return False
 
 
 #############################################################################################
@@ -263,7 +140,6 @@ def test0():
 def test1():
     cluster = Cluster({(-1, -2)})
     cluster.update()
-    #cluster.show()
     print('test1 {}'.format(cluster))
     #cluster.writeFile('test1.nc')
 
@@ -271,25 +147,21 @@ def testHorizLine():
     cluster = Cluster({(-1, -2), (0, -2), (1, -2), (2, -2)})
     cluster.update()
     print('testHorizLine {}'.format(cluster))
-    #cluster.show()
 
 def testDipole():
     cluster = Cluster({(-2, 0), (2, 0)})
     cluster.update()
     print('testDipole {}'.format(cluster))
-    #cluster.show()
 
 def testRectangle():
     cluster = Cluster({(i, 0) for i in range(3)}.union({(i, 1) for i in range(3)}))
     cluster.update()
     print('testRectangle {}'.format(cluster))
-    #cluster.show()
 
 def testRectangleSlanted():
     cluster = Cluster({(i, 0) for i in range(4)}.union({(i - 1, 1) for i in range(4)}))
     cluster.update()
     print('testRectangleSlanted {}'.format(cluster))
-    #cluster.show()
 
 def testRandom():
     import random
@@ -297,16 +169,7 @@ def testRandom():
     cluster = Cluster({(random.randint(0, 200), random.randint(0, 100)) for i in range(500)})
     cluster.update()
     print('testRandom {}'.format(cluster))
-    cluster.show()
-
-def testIfPointIsInsideEllipsis():
-    cluster = Cluster({(i, 0) for i in range(3)}.union({(i, 1) for i in range(3)}))
-    cluster.update()
-    assert(cl)
-    print('test {}'.format(cluster))
-    #cluster.show()
-    cluster = Cluster({(0,0), (1,0), (1,1), ()})
-
+    
 
 if __name__ == '__main__':
     test0()
