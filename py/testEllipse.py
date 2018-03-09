d1583855c390d632a4d541f9663aeb4b9e1da5ca
DavidCoppin <d.coppin@auckland.ac.nz>
2018-03-09 16:11:09 +1300

    Added opencv part (routine in opencv.py and code in testEllipses.py)

--- py/testEllipse.py
+++ py/testEllipse.py
@@ -1,6 +1,8 @@
 import numpy
 import cv2
 from ellipse import Ellipse
+from opencv import getContours, getEllipse
+import matplotlib.pyplot as mpl
 
 """
 Compare the ellipses obtained from Ellipse with those obtained from opencv3
@@ -23,7 +25,11 @@ for c in cells:
 ell = Ellipse(cells)
 
 # now do the same with cv2...
-# TO DO 
+test_result, test_contours = getContours(cellData)
+test_ellipse = getEllipse(test_result,test_contours)
+mpl.contourf(test_result)
+mpl.contour(test_ellipse)
+mpl.show()
 
 # compare the two ellipses
 # centre, axes, angle.... may be points?
