76da4b4ca2470978d5dc078c388d6126a02e2466
DavidCoppin <d.coppin@auckland.ac.nz>
2018-04-12 15:30:08 +1200

    Trying to implement mask in testCmorph (still problems)

--- py/testCmorph.py
+++ py/testCmorph.py
@@ -1,11 +1,12 @@
 from netCDF4 import Dataset as nc
 import argparse
 import numpy
-import matplotlib
+import matplotlib.pyplot as mpl
 import time
 from time_connected_clusters import TimeConnectedClusters
 from feature_extractor import FeatureExtractor
 from cluster import Cluster
+from coastal_mapping import CoastalMapping
 #from scipy import ndimage
 #from skimage.morphology import watershed
 #import cv2
@@ -13,12 +14,17 @@ import sys,os,string
 import bz2
 from datetime import datetime,timedelta as td
 
-def testCmorph(fyear, lyear, minmax_lons, minmax_lats, min_ellipse_axis, frac_ellipse, suffix, save=False):
+def testCmorph(lsm, fyear, lyear, minmax_lons, minmax_lats, reso, min_ellipse_axis, frac_ellipse, \
+                szone, lzone, min_size, max_size, suffix, save=False):
     """
     Checking that we can create a time connected cluster from image
     """
     lon_slice = slice(minmax_lons[0], minmax_lons[1])
     lat_slice = slice(minmax_lats[0], minmax_lats[1])
+    # Get the two coastal masks
+    print 'lsm', lsm
+    cm = CoastalMapping(lsm, numpy.int(reso), lat_slice, lon_slice, numpy.int(szone), \
+                         numpy.int(lzone), numpy.int(min_size), numpy.int(max_size))
     llat = minmax_lats[1] - minmax_lats[0]
     llon = minmax_lons[1] - minmax_lons[0]
     tcc = TimeConnectedClusters()
@@ -30,7 +36,7 @@ def testCmorph(fyear, lyear, minmax_lons, minmax_lats, min_ellipse_axis, frac_el
         filename=os.path.join('Data/CMORPH/Cmorph-' \
                + str(date.year) + '_' + str(date.month).zfill(2) + '_'\
                + str(date.day).zfill(2) + '.nc.bz2')
-        #Open the file
+        # Open the file
         filename = filename.replace('--','-').replace('__','_')
         print 'filename', filename
         zipfile = bz2.BZ2File(filename)
@@ -44,9 +50,20 @@ def testCmorph(fyear, lyear, minmax_lons, minmax_lats, min_ellipse_axis, frac_el
         for t in xrange(48) :
             print 'nb_day, t', nb_day, t
             data = f.variables["CMORPH"][t, lat_slice, lon_slice]
+            # Extract clusters with watershed
             clusters = FeatureExtractor(data, thresh_low=0., thresh_high=2.5).getClusters(min_ellipse_axis)
+            # Apply large mask to remove clusters too far away from islands
+            new_clusters = Clusters.isClusterInsideOf(clusters, cm.lArea, 0.9)
+            mpl.subplot(1,2,1)
+            mpl.contourf(np.flipud(clusters))
+            mpl.contour(np.flipud(cm.lArea))
+            mpl.subplot(1,2,2)
+            mpl.contourf(np.flipud(new_clusters))
+            mpl.contour(np.flipud(cm.lArea))
+            mpl.show()
+            sys.exit()
 #            print clusters
-            tcc.addTime(clusters,frac_ellipse)
+            tcc.addTime(new_clusters,frac_ellipse)
         os.remove(newfilename)
     # write to file
     lat = f.variables['lat'][minmax_lats[0]:minmax_lats[1]]
@@ -60,14 +77,28 @@ def testCmorph(fyear, lyear, minmax_lons, minmax_lats, min_ellipse_axis, frac_el
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Tracking.')
     parser.add_argument('-w', action='store_true', help='Print copyright')
-    parser.add_argument('-s', dest='save', action='store_true', help='Save time connected clusters object for debugging')
+    parser.add_argument('-save', dest='save', action='store_true', help='Save time connected clusters \
+                           object for debugging')
     parser.add_argument('-d1', dest='date1', default='2010-01-12', help='Start date YYYY-MM-DD')
     parser.add_argument('-d2', dest='date2', default='2010-01-13', help='End date YYYY-MM-DD')
-    parser.add_argument('-lons', dest='lons', default='1450:1700', help='Min and max longitude indices LONMIN,LONMAX')
-    parser.add_argument('-lats', dest='lats', default='300:550', help='Min and max latitude indices LATMIN,LATMAX')
-    parser.add_argument('-min_axis', dest='min_axis', type=float, default=6, help='Min ellipse axis in pixels')
-    parser.add_argument('-frac_ellipse', dest='frac_ellipse', type=float, default=0.8, help='Threshold to merge overlapping ellipses')
+    parser.add_argument('-lons', dest='lons', default='1450:1700', help='Min and max longitude \
+                           indices LONMIN,LONMAX')
+    parser.add_argument('-lats', dest='lats', default='300:550', help='Min and max latitude \
+                           indices LATMIN,LATMAX')
+    parser.add_argument('-min_axis', dest='min_axis', type=float, default=6, help='Min ellipse \
+                           axis in pixels')
+    parser.add_argument('-lsm', dest='lsm', default='Data/LSM/Cmorph_slm_8km.nc', help='path to \
+                           land-sea data')
+    parser.add_argument('-reso', dest='reso', default='8', help='resolution of the dataset in km')
+    parser.add_argument('-frac_ellipse', dest='frac_ellipse', type=float, default=0.8, \
+                           help='Threshold to merge overlapping ellipses')
     parser.add_argument('-suffix', dest='suffix', default='', help='suffix for output')
+    parser.add_argument('-sz', dest='sz', default='6', help='small distance to coast in pixels')
+    parser.add_argument('-lz', dest='lz', default='50', help='large distance to coast in pixels')
+    parser.add_argument('-smin', dest='smin', default='300', help='area below which islands \
+                           are deleted in km2')
+    parser.add_argument('-smax', dest='smax', default='800000', help='max area of filled \
+                           islands in km2')
     args = parser.parse_args()
 
     # get the lat-lon box
@@ -86,5 +117,7 @@ if __name__ == '__main__':
     except IndexError,ValueError:
         sys.stdout.write(helpstring+'\n')
         sys.exit()
-    testCmorph(fyear,lyear,minmax_lons, minmax_lats, args.min_axis, args.frac_ellipse, args.suffix, args.save)
+    testCmorph(args.lsm, fyear,lyear,minmax_lons, minmax_lats, args.min_axis, args.reso, \
+                args.frac_ellipse, args.suffix, args.sz, args.lz, args.smin, args.smax, \
+                args.save)
 
