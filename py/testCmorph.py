a0a75e088b2962257a0d2774091be8b3a116612f
DavidCoppin <d.coppin@auckland.ac.nz>
2018-04-24 16:29:27 +1200

    slowly solving errors

--- py/testCmorph.py
+++ py/testCmorph.py
@@ -38,8 +38,8 @@ def testCmorph(lsm, fyear, lyear, minmax_lons, minmax_lats, reso, min_ellipse_ax
     # Get the two coastal masks
     cm = CoastalMapping(lsm, np.int(reso), lat_slice, lon_slice, np.int(szone), \
                          np.int(lzone), np.int(min_size), np.int(max_size))
-    mpl.contourf(np.flipud(cm.sArea))
-    mpl.show()
+#    mpl.contourf(np.flipud(cm.sArea))
+#    mpl.show()
     llat = minmax_lats[1] - minmax_lats[0]
     llon = minmax_lons[1] - minmax_lons[0]
     tcc = TimeConnectedClusters()
@@ -81,7 +81,7 @@ def testCmorph(lsm, fyear, lyear, minmax_lons, minmax_lats, reso, min_ellipse_ax
     unit = f.variables["time"].units
     f.close()
     tcc.removeTracksByValidMask(valid_mask=np.flipud(cm.sArea), frac=frac_mask)
-    tcc.writeFile('cmorph.nc_'+str(suffix), unit, lat, lon, i_minmax=(0, len(lat)), \
+    of.writeFile('cmorph.nc_'+str(suffix), unit, lat, lon, i_minmax=(0, len(lat)), \
                    j_minmax=(0, len(lon)))
     if save:
         tcc.save('cmorph.pckl_'+str(suffix))
