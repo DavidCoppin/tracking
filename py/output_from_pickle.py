177fbe16dc106b93925414e5c4e7c02d4c02480e
David Coppin <david.ad.coppin@gmail.com>
2018-05-29 05:12:59 +0000

    Revert "Correction bug track_id"
    This reverts commit 01aed30e0ed5e871985ae5b4a0330f5045097c21

--- py/output_from_pickle.py
+++ py/output_from_pickle.py
@@ -1,3 +1,9 @@
+'''
+@description: A Class that selects the pickles needed for the output, extract tracks from
+              the pickles and put them with a unique in a 3D array that will be used as 
+              output and as a mask on precipitation, and write the final output
+'''
+
 import cPickle
 import numpy as np
 import netCDF4
@@ -7,14 +13,12 @@ import os
 import gzip
 import matplotlib.pyplot as mpl
 
-"""
-Write file with two variables: cprec (coastal precipitation) and nb (indexing of clusters)
-after the clusters have been transformed into arrays
-"""
 
 def createTxt(filename, list):
     """
-    Create a txt file
+    @param filename: file name of txt file to be written
+    @param list
+    @return txt file
     """
     info = open(filename, 'w')
     for item in list:
@@ -23,7 +27,8 @@ def createTxt(filename, list):
 
 def readTxt(filename):
     """
-    Read each line of a txt file to create a list
+    @param filename: file name of the file to be read
+    @return list of each line in the txt file
     """
     name = []
     with open(filename, 'r') as file:
@@ -54,27 +59,33 @@ class OutputFromPickle:
         # name of the pickle files to be read
         self.filenames = []
 
-        # Dictionary with name of pickle file as key, value is a np.array with same length as
+        # dictionary with name of pickle file as key, value is a np.array with same length as
         # number of tracks from pickle, set at 0 by default and replace by track Id when track
         # over several output files
         self.track_id = track_id
 
+        # latitude range of the final output
         self.lat = lat
 
+        # longitude range of the final output
         self.lon = lon
 
+        # id of the cluster
         self.id = id
 
+        # path where the pickles are stored
         self.inputdir = inputdir
 
+        # path where the output is stored
         self.outputdir = outputdir
 
+        # list of prefix of the different regions to include in the output
         self.list_prefix = list_prefix
 
 
     def selectPickles(self):
         """
-        Gather files from the same regions
+        @return list with all the pickles needed for a day
         """
         for n in range(len(self.list_prefix)):
             test = [i for i in os.listdir(self.inputdir)]
@@ -90,16 +101,17 @@ class OutputFromPickle:
 
     def extractTracks(self, files):
         """
-        Extract tracks that correspond to the time of the file
-        @param files: all the pickles files that will considered for this day
+        Extract tracks that correspond to the time of the file and gives them 
+        a unique id
+        @param files: all the pickles files for this day
         """
         self.clusters = np.zeros((self.end-self.ini, len(self.lat), len(self.lon)))
         tot_lon = len(self.lon)
         for i in files:
             lat_min, lat_max, lon_min, lon_max = self.getLatLon(i)
-#            print 'i, lat_min, lat_max, lon_min, lon_max', i, lat_min, lat_max, lon_min, lon_max
             with gzip.GzipFile(i) as gzf:
                 tracks = cPickle.load(gzf)
+
             # Set default track Id = 0 for all tracks if first time that file is read
             if len(self.track_id) == 0:
                 self.track_id = {i: np.zeros(len(tracks))}
@@ -107,70 +119,68 @@ class OutputFromPickle:
                 print 'i not in self.track_id', i
                 self.setTrackId(i, len(tracks))
             for nb in range(len(tracks)):
-                keys = sorted(tracks[nb].keys())
+                keys = tracks[nb].keys()
+
                 # Check if track has an Id different from 0
                 if self.track_id.get(i)[nb] > 0 :
                     new_id = self.track_id.get(i)[nb]
                 else :
                     new_id = self.id + 1
                     self.id = self.id + 1
-                # Fill in clusters with new_id where the track is
+
+                # Fill in clusters with new_id
                 for k in keys:
                     if k >= self.ini and k < self.end:
                         for cl in tracks[nb][k]:
                             i_index, j_index, mat = cl.toArray()
-#			    print 'mat', np.shape(mat), mat
                             if int(lon_min) < int(lon_max):
                                 self.clusters[k-self.ini, i_index[0]+int(lat_min):i_index[-1]\
                                                +int(lat_min)+1,j_index[0]+int(lon_min):j_index[-1]\
                                                +int(lon_min)+1][np.where(mat==1)]= new_id
-                            # Africa
+
+                            # Special case for region on both sides of 0 degree longitude
                             else :
-				len_west = tot_lon - int(lon_min)
-#				print 'lon_end, len_lon_africa, i_index, j_index', \
-#                                        lon_end, len_lon_africa, i_index, j_index
-				# Part from 0 to 335
+                                len_west = tot_lon - int(lon_min)
+
+                                # Only positive longitudes
                                 if j_index[0] > len_west :
-#				    print 'part east'
-				    self.clusters[k-self.ini, i_index[0]+int(lat_min):i_index[-1]\
+                                    self.clusters[k-self.ini, i_index[0]+int(lat_min):i_index[-1]\
                                                    +int(lat_min)+1,j_index[0]-len_west:j_index[-1]\
                                                    -len_west+1][np.where(mat==1)]= new_id
-				# Part from 4497 to 4948
-				elif j_index[-1] < len_west :
-#				    print 'part west'
-#				    print 'j_index[0]+int(lon_min), j_index[-1]+int(lon_min)+1',\
-#					    j_index[0]+int(lon_min), j_index[-1]+int(lon_min)+1
-				    self.clusters[k-self.ini, i_index[0]+int(lat_min):i_index[-1]\
+
+                                # Only negative longitudes
+                                elif j_index[-1] < len_west :
+                                    self.clusters[k-self.ini, i_index[0]+int(lat_min):i_index[-1]\
                                                    +int(lat_min)+1,j_index[0]+int(lon_min):j_index[-1]\
                                                    +int(lon_min)+1][np.where(mat==1)]= new_id
-				# On both parts
-				else :
-#				    print 'both parts'
-				    mat_west = mat[:,0:(len_west-j_index[0])]
-				    mat_east = mat[:,(len_west-j_index[0]):]
-#                                    print 'np.shape(mat), np.shape(mat_east), np.shape(mat_west)', \
-#					np.shape(mat), np.shape(mat_east), np.shape(mat_west)
-				    self.clusters[k-self.ini, i_index[0]+int(lat_min):i_index[-1]\
+
+                                # Both
+                                else :
+                                    mat_west = mat[:,0:(len_west-j_index[0])]
+                                    mat_east = mat[:,(len_west-j_index[0]):]
+                                    self.clusters[k-self.ini, i_index[0]+int(lat_min):i_index[-1]\
                                                    +int(lat_min)+1,0:len(j_index)\
-						   -(len_west-j_index[0])+1]\
-						   [np.where(mat_east==1)]= new_id
-				    self.clusters[k-self.ini, i_index[0]+int(lat_min):i_index[-1]\
+                                                   -(len_west-j_index[0])+1]\
+                                                   [np.where(mat_east==1)]= new_id
+                                    self.clusters[k-self.ini, i_index[0]+int(lat_min):i_index[-1]\
                                                    +int(lat_min)+1,j_index[0]+int(lon_min):]\
                                                    [np.where(mat_west==1)]= new_id
-                            # Replace track ID kept for next output file if track goes further \
-                            # than future output
+
+                            # Replace track ID kept for next output file if track goes further
+                            # than end of this day
                             if keys[-1] >= self.end-self.ini:
                                 self.track_id[i][nb] = new_id
-                # if not used, do not waste id
+
+                # if id not used, do not waste id
                 if keys[-1] < self.ini or keys[0] >= self.end:
                     self.id = self.id -1
                     new_id = self.id - 1
-#               print 'i, nb, self.track_id[i][nb]', i, nb, self.track_id[i][nb]
 
 
     def getLatLon(self, file):
         """
-        Get latitude and longitude to place area into global file
+        @param file: pickle file
+        @return latitude and longitude to place area into global file
         """
         for i in self.list_prefix:
             if i in file:
@@ -185,21 +195,21 @@ class OutputFromPickle:
         """
         If first time reading pickle file, add it to dictionary and set track id to 0 for
         every track
+        @param filename: pickle file
+        @param nb_tracks: number of tracks in pickle file
         """
         self.track_id[filename] = np.zeros(nb_tracks)
-#        self.track_id.append({filename: np.zeros(nb_tracks)})
 
 
     def deletePickles(self):
         """
-        Delete pickle once they are all used
+        Delete pickle once all its tracks are finished
         """
         for nb in range(len(self.filenames)):
             num = [int(s) for s in self.filenames[nb].split('_') if s.isdigit()]
             if num[1] < self.end :
                 print 'should delete self.filenames[nb]', self.filenames[nb]
                 os.remove(self.filenames[nb])
-                # Delete track_id associated with pickle
                 self.deleteTrackId(self.filenames[nb])
 
 
@@ -213,10 +223,8 @@ class OutputFromPickle:
     def writeFile(self, suffix, old_filename):
         """
         Write data to netcdf file
-        @param new_filename new file name
-        @param latitude
-        @param longitude
-        @param ini and end: indicates the timesteps when the data needs to be written
+        @param suffix: suffix for output
+        @param old_filename: name of cmorph file corresponding
         """
         new_filename = str(self.outputdir)+'tracking'+str(old_filename[-18:-7])+'_'\
                         +str(suffix)+'.nc'
@@ -225,7 +233,6 @@ class OutputFromPickle:
         # create dimensions
         num_i = len(self.lat)
         f.createDimension('lat', size=num_i)
-
         num_j = len(self.lon)
         f.createDimension('lon', size=num_j)
 
@@ -242,13 +249,6 @@ class OutputFromPickle:
         except RuntimeError:
             ori = nc(filename.replace('-','_'))
         var = ori.variables["CMORPH"][:,:,:]
-#        if lon_min < lon_max:
-#            var = ori.variables["CMORPH"][:, int(lat_min):int(lat_max), int(lon_min):int(lon_max)]
-#        else:
-#            var1 = ori.variables["CMORPH"][:, int(lat_min):int(lat_max), int(lon_min):]
-#            var2 = ori.variables["CMORPH"][:, int(lat_min):int(lat_max), :int(lon_max)]
-#            var = np.concatenate((var1, var2), axis=2)
-#            del var1, var2
         tint = ori.variables["time"][:]
         unit = ori.variables["time"].units
         os.remove(filename)
@@ -291,11 +291,11 @@ class OutputFromPickle:
         f.variables['nb'].short_name='nb'
         f.variables['nb'].units='no unit'
 
-
         i_index[:] = self.lat[:]
         j_index[:] = self.lon[:]
         t_index[:] = tint[:]
-        # now write all the data in one go
+
+        # Write data for whole day
         mask=np.zeros((self.end-self.ini, num_i, num_j))
         mask[np.where(self.clusters != 0)] = 1
         precip[:] = var * mask
@@ -305,19 +305,3 @@ class OutputFromPickle:
         f.close()
 
 
-###################
-def testWrite():
-        id = 0
-        ini = 0
-        end = 40
-        on = OutputNetcdf(ini, end)
-        files = on.selectPickles('pickles/png')
-        print 'files', files
-        on.extractTracks(files, 300, 500, id)
-#        on.deletePickles()
-#        on.writeFile(str(suffix), list_filename[nb_day], lat, lon, \
-#                              unit, lat_slice, lon_slice)
-
-
-if __name__ == '__main__':
-    testWrite()
