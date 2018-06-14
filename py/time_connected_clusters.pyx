6194fee6ce18776f965eed59a35d5432aacc09c3
DavidCoppin <d.coppin@auckland.ac.nz>
2018-06-14 17:21:44 +1200

    Typo

--- py/time_connected_clusters.pyx
+++ py/time_connected_clusters.pyx
@@ -404,9 +404,10 @@ class TimeConnectedClusters:
             if self.t_index in self.cluster_connect[track_id] and \
                     self.t_index -1 in self.cluster_connect[track_id] :
                 # compare area
-                if self.cluster_connect[track_id][self.t_index]['area'] < \
-                        (1-frac) * self.cluster_connect[track_id][self.t_index - 1]['area'] \#:
-                        and self.cluster_connect[track_id][self.t_index-1]['area'] > 490:
+                if self.cluster_connect[track_id][self.t_index-1]['area'] > 490 and \
+                        (self.cluster_connect[track_id][self.t_index]['area'] < \
+#               if (self.cluster_connect[track_id][self.t_index]['area'] < \
+                        (1-frac) * self.cluster_connect[track_id][self.t_index - 1]['area']):
                     # create new track with track at t_index and delete it at t_index -1
                     print 'self.cluster_connect[track_id][self.t_index-1][area]', \
                             self.cluster_connect[track_id][self.t_index-1]['area']
